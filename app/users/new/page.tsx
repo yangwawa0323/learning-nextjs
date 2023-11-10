750c3eb38e025dfb11842ed8a93848b4f61e10aa
yangwawa0323 <yangwawa0323@163.com>
2023-11-11 02:02:20 +0800

    Static and dynamic rendering

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NewUserPage = () => {
-	return <div>New User Page</div>;
+	return (
+		<div>
+			<h1>New User Page</h1>
+			<Link href='/users'>Go back to /users</Link>
+		</div>
+	);
 };
 
 export default NewUserPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -14,12 +14,15 @@ interface User {
 }
 
 const UsersPage = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+		cache: 'no-cache',
+	});
 	const users: User[] = await res.json();
 	return (
 		<div>
 			<h1>Users Page</h1>
 			<Link href='/users/new'> New user</Link>
+			<p>{new Date().toLocaleTimeString()}</p>
 			<ul>
 				{users.map((user, idx) => {
 					return <li key={idx}>{user.company.name}</li>;
